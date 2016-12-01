dff4e85e7632ff75841227e69fdb2a6ddcf246e6
Tammy <tammym@promodel.com>
2016-12-01 08:42:52 -0500

    linting cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,9 @@ export class AppComponent {
   fromArabic(n) {
     let r = '',
     decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
-    roman = ['M', 'CM','D','CD','C', 'XC', 'L', 'XL', 'X','IX','V','IV','I'];
+    roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
     for (let i = 0; i < decimals.length; i++) {
         while (n >= decimals[i]) {
-          console.log(r);
-          console.log(n);
             r += roman[i];
             n -= decimals[i];
         }
@@ -27,11 +25,11 @@ export class AppComponent {
 fromRoman(str) {
   let result = 0;
   let decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  let roman = ['M', 'CM','D','CD','C', 'XC', 'L', 'XL', 'X','IX','V','IV','I'];
-  for (let i = 0;i<=decimal.length;i++) {
-    while (str.indexOf(roman[i]) === 0){
+  let roman = ['M', 'CM', 'D', 'CD', ' C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  for (let i = 0; i <= decimal.length; i++) {
+    while (str.indexOf(roman[i]) === 0) {
       result += decimal[i];
-      str = str.replace(roman[i],'');
+      str = str.replace(roman[i], '');
     }
   }
   return result;
